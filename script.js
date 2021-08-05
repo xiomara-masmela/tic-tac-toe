8397aee68487368672ec32a092a8111c48def2d1
xiomara-masmela <xiomara-masmela@gmail.com>
2021-08-05 10:57:01 +1000

    Styling completed V1 and Js completed V1

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const board = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8]
-];
+// const board = [
+//     [0, 1, 2],
+//     [3, 4, 5],
+//     [6, 7, 8]
+// ];
 
 const winningMoves = [
     [0, 1, 2],
@@ -18,18 +18,36 @@ const winningMoves = [
 
 const game = {
     player01: {
-        player: "X",
+        player: "",
         plays: 0,
         moves: []
     },
     player02: {
-        player: "O",
+        player: "",
         plays: 0,
         moves: []
     },
-    isActive: true
+    isActive: true,
+    rounds: 0
 
 }
+
+let currentPlayer = "";
+//Manipulate the Game object
+function newNames() {
+    game.player01.player = document.getElementById('p1').value;
+    game.player02.player = document.getElementById('p2').value;
+    //console.log(game.player01.player ,game.player02.player );
+    currentPlayer = game.player01.player;
+    //Start the Game with Player 01
+    playerdiv.textContent = `Player: ${currentPlayer} starts`;
+
+    startGame();
+    return;
+}
+
+
+
 let gameState = ["", "", "", "", "", "", "", "", ""];
 
 
@@ -37,12 +55,17 @@ let gameState = ["", "", "", "", "", "", "", "", ""];
 const boardDiv = document.getElementById('game');
 const cells = document.getElementsByClassName('cell');
 const playerdiv = document.getElementById('player');
+const resetButton = document.getElementById('reset');
+
+
+
+
+
 //Js Variables
-let currentPlayer = game.player01.player;
+
 let gameActive = game.isActive;
 
-//Start the Game with Player X
-playerdiv.textContent = `Player: ${currentPlayer} starts`;
+
 
 //1. Check that the cell is clicked
 function clickOnCell() {
@@ -50,7 +73,8 @@ function clickOnCell() {
     const clickedCellIndex = clickedCell.dataset.index;
     cellPlayed(clickedCell, clickedCellIndex);
     resultValidation();
-    //cell.removeEventListener('click', clickOnCell);
+    clickedCell.removeEventListener('click', clickOnCell);
+    return;
 }
 //2. Change the current player
 function changePlayer() {
@@ -66,24 +90,30 @@ function changePlayer() {
 
 //3. Cell played: update internal gameState and updateUI
 function cellPlayed(clickedCell, clickedCellIndex) {
-
     gameState[clickedCellIndex] = currentPlayer;
     const cellContent = document.createElement('p');
     cellContent.classList.add('move');
     cellContent.textContent = currentPlayer;
     clickedCell.append(cellContent);
-
+    
+    if(currentPlayer === game.player01.player){
+        clickedCell.classList.add('clickedP1');
+    }
+    if(currentPlayer === game.player02.player) {
+        clickedCell.classList.add('clickedP2');
+    }
 }
 
 //4. resultsValidation - winner or draw
 function resultValidation() {
     let roundWon = false;
+    let rounds = game.rounds;
     //Winner
     for (move of winningMoves) {
         let a = gameState[move[0]];
         let b = gameState[move[1]];
         let c = gameState[move[2]];
-        console.log("a"+a,"b"+ b,"c"+ c)
+        //console.log("a"+a,"b"+ b,"c"+ c)
         if (a === '' || b === '' || c === '') {
             continue;
         }
@@ -94,8 +124,15 @@ function resultValidation() {
     }
 
     if (roundWon) {
-        playerdiv.textContent = `Player ${currentPlayer} wins`;
+        playerdiv.textContent = `${currentPlayer} wins`;
+        if(currentPlayer === game.player01.player){
+            playerdiv.classList.add('winner-P1');
+        }
+        if(currentPlayer === game.player02.player){
+            playerdiv.classList.add('winner-P2');
+        }
         gameActive = false;
+        resetButton.classList.add('visible');
         return;
     }
     //Draw
@@ -104,29 +141,48 @@ function resultValidation() {
     if (roundDraw) {
         playerdiv.textContent = `Draw!`;
         gameActive = false;
+        resetButton.classList.add('visible');
         return;
     }
     changePlayer();
+    
 }
 
-//4. Reset Game
-function resetGame(){
+//5. Reset Game
+function resetGame() {
+    //Variables back to initial state
     currentPlayer = game.player01.player;
     gameActive = game.isActive;
     gameState = ["", "", "", "", "", "", "", "", ""];
     playerdiv.textContent = "";
+    playerdiv.classList.remove('winner-P1');
+    playerdiv.classList.remove('winner-P2');
+
     const cellsContent = document.getElementsByClassName('move');
-    for (cellContent of cellsContent){
+    for (cellContent of cellsContent) {
         cellContent.textContent = "";
+    }
+    //Remove Styling on Game
+    const cells = document.getElementsByClassName('cell');
+    for (cell of cells){
+        cell.classList.remove('clickedP2');
+        cell.classList.remove('clickedP1');
+    }
+    //Clear input values
+    document.getElementById('p1').value = "";
+    document.getElementById('p2').value = "";
+    //Make Dissapear Reset Button
+    resetButton.classList.remove('visible');
+    resetButton.classList.add('invisible');
+}
+
+//Event listeners
+function startGame() {
+    for (cell of cells) {
+        cell.addEventListener('click', clickOnCell);
     }
 }
 
 
-//Event listeners
 
-for (cell of cells) {
-    cell.addEventListener('click', clickOnCell);
-}
-
-const resetButton = document.getElementById('reset');
 resetButton.addEventListener('click', resetGame);
